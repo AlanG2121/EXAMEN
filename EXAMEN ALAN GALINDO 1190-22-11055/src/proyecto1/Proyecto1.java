28303d2ee8cf9772ba34fa538920451db87484d4
AlanG2121 <158762979+AlanG2121@users.noreply.github.com>
2024-03-09 17:57:45 -0600

    Add files via upload

---
+++ EXAMEN ALAN GALINDO 1190-22-11055/src/proyecto1/Proyecto1.java
@@ -0,0 +1,33 @@
+
+package proyecto1;
+
+
+import Vista.Sistema;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+public class Proyecto1 {
+    
+    
+  
+
+   
+    public static void main(String[] args) throws SQLException {
+        
+        
+        Sistema log = new Sistema();
+       
+       log.setVisible(true);//para poner visible la ventana
+       
+        
+        
+    }
+
+    private PreparedStatement PreparedStatement(String scString) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
